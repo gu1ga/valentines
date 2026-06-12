7b37b9f6031f7877b32a9952e4eda8cf1e5b1ad0
Guilherme Gomes <guigagmg@gmail.com>
2026-06-12 01:21:06 -0300

    Updates

--- server/assets/_tanstack-start-manifest_v-CoMbjDVU.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/home/gu1ga/projects/valentines2/src/routes/__root.tsx", children: ["/", "/rail"], preloads: ["/assets/index-GYlifNlN.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-GYlifNlN.js" } }] }, "/": { filePath: "/home/gu1ga/projects/valentines2/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-uXz0spS8.js", "/assets/memories-B5O5aSCh.js"] }, "/rail": { filePath: "/home/gu1ga/projects/valentines2/src/routes/rail.tsx", children: void 0, preloads: ["/assets/rail-BJD8G5s0.js", "/assets/memories-B5O5aSCh.js"] } } });
-export {
-  tsrStartManifest
-};
